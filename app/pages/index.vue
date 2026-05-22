8ff47e968c59ba780500ebb14813f00f88da7bd4
silent_mantra <silentmantra.ru@gmail.com>
2026-05-22 23:44:32 +0200

    cleanup

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,14 +2,63 @@
 let cleanupLinks: (() => void) | undefined;
 
 onMounted(async () => {
-    import('./index/logo');
-    const { mountHeartbeatLinks } = await import('./index/links');
-    cleanupLinks = mountHeartbeatLinks();
+
+    const element = document.querySelector('.logo') as HTMLDivElement;
+
+    setInterval(() => {
+        const active = Math.random() < 0.05;
+
+        if (!active) {
+            resetEffect();
+            return;
+        }
+
+        const blur = random(0, 3);
+        const brightness = random(1, 2);
+        const contrast = random(0.8, 2.5);
+        const hue = random(-40, 40);
+
+        const s = random(.2, .5);
+        const translateX = random(-8 * s, 8 * s);
+        const scaleY = random(1 - .1 * s, 1 + .1 * s);
+        const opacity = random(0.75, 1);
+
+        element.style.filter = [
+            `blur(${blur.toFixed(2)}px)`,
+            `brightness(${brightness.toFixed(2)})`,
+            `contrast(${contrast.toFixed(2)})`,
+            `hue-rotate(${hue.toFixed(2)}deg)`
+        ].join(' ');
+
+        element.style.transform = [
+            `translateX(${translateX.toFixed(2)}px)`,
+            `scaleY(${scaleY.toFixed(3)})`
+        ].join(' ');
+
+        element.style.opacity = opacity.toFixed(2);
+
+        const duration = random(40, 180);
+
+        setTimeout(resetEffect, duration);
+
+    }, 120);
+
+    function resetEffect() {
+        element.style.filter = '';
+        element.style.transform = '';
+        element.style.opacity = '';
+    }
+
+    function random(min: number, max: number) {
+        return Math.random() * (max - min) + min;
+    }
+    //const { mountHeartbeatLinks } = await import('./index/links.client');
+    //cleanupLinks = mountHeartbeatLinks();
 });
 
 onBeforeUnmount(() => {
-    cleanupLinks?.();
-    cleanupLinks = undefined;
+    //cleanupLinks?.();
+    //cleanupLinks = undefined;
 });
 
 </script>
@@ -98,6 +147,7 @@ main {
         opacity 40ms linear;
 
     img {
+        aspect-ratio: 16 / 9;
         width: 100%;
         animation: logo-animate 3s infinite alternate;
 
@@ -123,9 +173,6 @@ main {
 
 
 }
-
-
-
 
 .tools {
     display: flex;
@@ -280,6 +327,8 @@ main {
 .links canvas {
     position: absolute;
     inset: 0;
+    width: 100%;
+    height: 100%;
     background: transparent;
     pointer-events: none;
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    ssr: true,
+
+    nitro: {
+        prerender: {
+            crawlLinks: true,
+        }
+    },
+
+    routeRules: {
+        '/stereo-reader/**': {
+            prerender: false
+        }
+    },
+
+    sourcemap: {
+        server: false,
+        client: false
+    },
+
+    vite: {
+        build: {
+            sourcemap: false
+        }
+    }
+});
