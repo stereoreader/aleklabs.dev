293ce696855a88bb51eaff9ddca10b6a710af223
silent_mantra <silentmantra.ru@gmail.com>
2026-06-13 11:35:16 +0200

    add app redirect

---
+++ app/pages/app/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const targetUrl = 'https://aleklabs.dev/stereo-reader/app/';
+
+useHead({
+    title: 'Redirecting...',
+    link: [
+        {
+            rel: 'canonical',
+            href: targetUrl
+        }
+    ],
+    meta: [
+        {
+            'http-equiv': 'refresh',
+            content: `0; url=${targetUrl}`
+        }
+    ]
+});
+
+await navigateTo(targetUrl, {
+    external: true,
+    redirectCode: 301
+});
+</script>
+
+<template>
+    <p>
+        Redirecting to
+        <a :href="targetUrl">{{ targetUrl }}</a>.
+    </p>
+</template>
