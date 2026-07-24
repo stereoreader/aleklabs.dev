bf2722de5c68802111f10548b49a03ff03bc2034
silent_mantra <silentmantra.ru@gmail.com>
2026-07-24 22:35:54 +0200

    cleanup

--- app/utils/common.ts
+++ app/utils/common.ts
@@ -1,5 +1,3 @@
 export function viewTransitionName(prefix: string, suffix: string) {
-    const out= prefix.replace(/[^\d\w]+/g, '-').replace(/^-+|-+$/g, '') + '-' + suffix;
-    console.log(out);
-    return out;
+    return prefix.replace(/[^\d\w]+/g, '-').replace(/^-+|-+$/g, '') + '-' + suffix;
 }
