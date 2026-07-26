b6f953a7ad391257bc204afb7109d541331a3176
silent_mantra <silentmantra.ru@gmail.com>
2026-07-26 11:58:46 +0200

    move SO links into <links>

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,6 +4,7 @@ import CardCmp from './index/card.vue';
 
 import ExperienceCmp from './index/experience.vue';
 import ArticlesCmp from './index/articles.vue';
+import LinksCmp from './index/links.vue';
 
 import stereoReaderPreview from './assets/stereo-reader-preview.jpg';
 import youtubeChatPreview from './assets/youtube-send-to-ai-chat.png';
@@ -21,43 +22,8 @@ useSeoMeta({
 </script>
 
 <template>
-    <al-page-cover :image-src="coverImg" text="ALEK LABS"  />
-    <div class="links">
-        <canvas id="heartbeat"></canvas>
-        <a href="https://www.linkedin.com/in/alexander-nenashev-930731288/" target="_blank">
-            <img class="icon-scale-80" src="./assets/icons/linkedin.svg">
-            <span>My LinkedIn profile</span>
-        </a>
-        <a href="https://stackoverflow.com/users/14098260/alexander-nenashev" target="_blank">
-            <img class="icon-scale-80" src="./assets/icons/stackoverflow.svg">
-            <span>My Stackoverflow profile</span>
-        </a>
-        <a href="https://stackoverflow.com/search?tab=votes&q=user%3a14098260%20%5bvue.js%5d%20or%20%5bvuejs3%5d&searchOn=3"
-            target="_blank">
-            <img src="./assets/icons/vue.svg">
-            <span>My Vue SO answers</span>
-        </a>
-        <a href="https://stackoverflow.com/search?tab=votes&q=user%3a14098260%20%5bvite%5d%20&searchOn=3"
-            target="_blank">
-            <img class="icon-scale-90" src="./assets/icons/vite.svg">
-            <span>My Vite SO answers</span>
-        </a>
-        <a href="https://stackoverflow.com/search?tab=votes&q=user%3a14098260%20%5btypescript%5d&searchOn=3"
-            target="_blank">
-            <img src="./assets/icons/typescript.svg">
-            <span>My Typescript SO answers</span>
-        </a>
-        <a href="https://stackoverflow.com/search?tab=votes&q=user%3a14098260%20%5bjavascript%5d&searchOn=3"
-            target="_blank">
-            <img src="./assets/icons/javascript.svg">
-            <span>My Javascript SO answers</span>
-        </a>
-        <a href="https://stackoverflow.com/search?tab=votes&q=user%3a14098260%20%5bcss%5d&searchOn=3"
-            target="_blank">
-            <img class="icon-scale-85 icon-nudge-down" src="./assets/icons/css.svg">
-            <span>My CSS SO answers</span>
-        </a>
-    </div>
+    <al-page-cover :image-src="coverImg" text="ALEK LABS" />
+    <links-cmp />
     <section class="tools" aria-label="Production experience">
         <h2><al-icon icon="experience" />Production experience</h2>
         <experience-cmp />
@@ -151,113 +117,4 @@ useSeoMeta({
     }
 }
 
-.links {
-    --heartbeat-bpm: 60;
-    --heartbeat-duration: calc(60s / var(--heartbeat-bpm));
-    position: relative;
-    justify-content: center;
-    display: flex;
-    gap: 32px;
-    margin-bottom: 32px;
-}
-
-.links a {
-    filter: saturate(0);
-    opacity: .7;
-    --border-radius: calc(var(--border-radius) / 2);
-    --overflow: hidden;
-    width: 64px;
-    height: 64px;
-    transition: transform .3s;
-}
-
-.links a span {
-    display: none;
-}
-
-.links a:hover {
-    filter: saturate(var(--saturate-base));
-    opacity: 1;
-    transform: scale(1.1);
-    background-color: transparent;
-    animation: glow var(--heartbeat-duration) infinite linear;
-    filter: drop-shadow(0 0 12px #4fc3ff);
-    animation-delay: calc(var(--heartbeat-duration) * 0.07);
-}
-
-.links a:hover span {
-    font-size: 12px;
-    color: #bbb;
-    display: block;
-    position: absolute;
-    top: 64px;
-    width: 300px;
-    left: 50%;
-    transform: translateX(-50%);
-    text-align: center;
-}
-
-
-@keyframes glow {
-    0% {
-        filter:
-            saturate(var(--saturate-base)) drop-shadow(0 0 12px #4fc3ff);
-    }
-
-    8% {
-        filter:
-            saturate(calc(var(--saturate-base) + (1 - var(--saturate-base)) * 0.5)) drop-shadow(0 0 18px #a88cb3);
-    }
-
-    16% {
-        filter:
-            saturate(calc(var(--saturate-base) + (1 - var(--saturate-base)) * 0.2)) drop-shadow(0 0 13px #6bb7eb);
-    }
-
-    24% {
-        filter:
-            saturate(1) drop-shadow(0 0 24px #ff0000);
-    }
-
-    36% {
-        filter:
-            saturate(calc(var(--saturate-base) + (1 - var(--saturate-base)) * 0.3)) drop-shadow(0 0 14px #4fc3ff);
-    }
-
-    100% {
-        filter:
-            saturate(var(--saturate-base)) drop-shadow(0 0 12px #4fc3ff);
-    }
-}
-
-.links a img {
-    width: 100%;
-    object-fit: contain;
-}
-
-.links canvas {
-    position: absolute;
-    inset: 0;
-    width: 100%;
-    height: 100%;
-    background: transparent;
-    pointer-events: none;
-}
-
-.icon-scale-80 {
-    transform: scale(.8);
-}
-
-.icon-scale-85 {
-    transform: scale(.85);
-}
-
-.icon-scale-90 {
-    transform: scale(.9);
-}
-
-.icon-nudge-down {
-    position: relative;
-    top: 2px;
-}
 </style>
