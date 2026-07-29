1dcfa29bee7e3b628f7dc0a94d30181c649c9ecc
silent_mantra <silentmantra.ru@gmail.com>
2026-07-29 17:09:17 +0200

    move GA into nuxt plugin to avoid LCP clash

---
+++ app/plugins/google-analytics.client.ts
@@ -0,0 +1,49 @@
+declare global {
+    interface Window {
+        dataLayer: unknown[][];
+        gtag: (...args: unknown[]) => void;
+    }
+}
+
+export default defineNuxtPlugin(() => {
+    const measurementId = 'G-CWXQF8PH4S';
+    let started = false;
+
+    if (document.readyState === 'complete') {
+        scheduleStart();
+    } else {
+        window.addEventListener('load', scheduleStart, { once: true });
+    }
+
+    function scheduleStart() {
+        requestAnimationFrame(() => {
+            requestAnimationFrame(() => {
+                if ('requestIdleCallback' in window) {
+                    window.requestIdleCallback(startAnalytics, { timeout: 3000 });
+                    return;
+                }
+
+                window.setTimeout(startAnalytics, 1000);
+            });
+        });
+    }
+
+    function startAnalytics() {
+        if (started) return;
+
+        started = true;
+        window.dataLayer = [];
+        window.gtag = function gtag(...args: unknown[]) {
+            window.dataLayer.push(args);
+        };
+
+        window.gtag('js', new Date());
+        window.gtag('config', measurementId);
+
+        const script = document.createElement('script');
+
+        script.async = true;
+        script.src = `https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(measurementId)}`;
+        document.head.append(script);
+    }
+});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -156,22 +156,6 @@ export default defineNuxtConfig({
     app: {
         viewTransition: 'always',
         head: {
-            script: [
-                {
-                    async: true,
-                    src: 'https://www.googletagmanager.com/gtag/js?id=G-CWXQF8PH4S',
-                },
-                {
-                    innerHTML: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', 'G-CWXQF8PH4S', {
-                            product_surface: 'website'
-                        });
-                    `,
-                },
-            ],
         },
     },
 
